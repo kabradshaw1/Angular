1102bf854cac294013b82bb36a01b397e0570b78
kabradshaw1 <itllbok82@gmail.com>
2023-09-14 16:47:28 -0400

    removed header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ProductListComponent
   ],
   imports: [
